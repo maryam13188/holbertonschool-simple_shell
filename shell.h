ffbcf9b427b72aea573e4691b42794bdcd46c2ef
ReemAlshehri04 <re2malshehri@gmail.com>
2025-12-28 17:50:52 +0000

    Added shell_loop() function

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,22 +7,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
 /* Function Prototypes */
 int is_whitespace(const char *str);
 char *read_line(void);
 char **split_line(char *line);
 void free_tokens(char **tokens);
 int execute(char **args);
-
 /* String Functions */
 int _strlen(char *s);
 char *_strdup(char *str);
-
 /* PATH Functions */
 char *get_path_from_environ(void);
-
+int check_command(char *command);
+char *find_full_path(char *command);
 /* Environment */
 extern char **environ;
-
 #endif
