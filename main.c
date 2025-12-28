80577e87fed096939f570e1af89fba6c21133d00
ReemAlshehri04 <re2malshehri@gmail.com>
2025-12-28 17:52:15 +0000

    Calls shell_loop() to start the shell

--- main.c
+++ main.c
@@ -1,47 +1,40 @@
 #include "shell.h"
-
 /**
- * main - Entry point for simple shell
- * Return: Always 0
- */
+	* main - Entry point for simple shell
+	* Return: Always 0
+	*/
 int main(void)
 {
-    char *line;
-    char **args;
-
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("($) ");
-
-        line = read_line();
-        if (!line)
-        {
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
-
-        /* Check if line is only whitespace */
-        if (is_whitespace(line))
-        {
-            free(line);
-            continue;
-        }
-
-        args = split_line(line);
-        if (!args || !args[0]) /* No commands */
-        {
-            free(line);
-            free_tokens(args);
-            continue;
-        }
-
-        execute(args);
-
-        free(line);
-        free_tokens(args);
-    }
-
-    return (0);
+char *line;
+char **args;
+int status = 1;
+while (status)
+{
+if (isatty(STDIN_FILENO))
+printf("($) ");
+line = read_line();
+if (!line)
+{
+if (isatty(STDIN_FILENO))
+printf("\n");
+break;
+}
+/* Check if line is only whitespace */
+if (is_whitespace(line))
+{
+free(line);
+continue;
+}
+args = split_line(line);
+if (!args || !args[0]) /* No commands */
+{
+free(line);
+free_tokens(args);
+continue;
+}
+status = execute(args);
+free(line);
+free_tokens(args);
+}
+return (0);
 }
